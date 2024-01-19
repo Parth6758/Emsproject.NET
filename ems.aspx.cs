df820f94c8731f41308f5e8ee316a7c6a25cff2f
Parth9811 <parthkotadiya666@gmail.com>
2024-01-19 15:51:07 +0530

    Database added with config file

--- ems.aspx.cs
+++ ems.aspx.cs
@@ -6,17 +6,46 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Web.Services.Description;
 
 namespace Emsproject
 {
     public partial class ems : System.Web.UI.Page
     {
+        SqlConnection conn;
+        public void fnConnection()
+        {
+
+            try
+            {
+                String constr = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+                conn = new SqlConnection(constr);
+                if (conn.State != ConnectionState.Open)
+                
+                    conn.Open();
+                
+                Console.WriteLine("Database Connected Succesfully");
+
+
+            }
+            catch (Exception e)
+            {
+                Response.Write(e.ToString());
+            }
+        }
+        
+       
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!Page.IsPostBack)
             {
                 fnBindtype();
             }
+
+            
         }
 
         protected void Submit_Click(object sender, EventArgs e)
